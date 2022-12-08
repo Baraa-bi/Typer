f329446a20b9f72cc62a11a24500ea9f8642baa8
Baraa <baraabilal8@gmail.com>
2022-12-08 13:29:00 -0600

    adding word animation

--- components/Word.tsx
+++ components/Word.tsx
@@ -6,11 +6,13 @@ interface Props {
 
 export default function Word(props: Props) {
   const { word, isSelected, isCorrect } = props;
-  const textColor = isCorrect ? "green" : isCorrect === false ? "red" : "gray";
-  const backgroundColor = isSelected ? "bg-gray-100" : "bg-white";
+  const textColor = isCorrect ? "green" : isCorrect === false ? "red" : "black";
+  const backgroundColor = isSelected
+    ? "bg-gray-100 transition scale-110 duration-200 ease-in-out"
+    : "bg-white";
   return (
     <div
-      class={`text-3xl font-medium py-2 rounded col-span-1 ${backgroundColor}`}
+      class={`text-2xl font-medium rounded py-2 col-span-1 ${backgroundColor}`}
     >
       <span class={`${`text-${textColor}-500`}`}>{word}</span>
     </div>
